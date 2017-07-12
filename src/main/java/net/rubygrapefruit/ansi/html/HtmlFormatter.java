a3934199ad4c289df6ef4cc79347b4f3e3c95e50
Adam Murdoch <adam@gradle.com>
2017-07-13 09:57:43 +1200

    Changed `HtmlFormatter` to render bold text.

--- src/main/java/net/rubygrapefruit/ansi/html/HtmlFormatter.java
+++ src/main/java/net/rubygrapefruit/ansi/html/HtmlFormatter.java
@@ -1,42 +1,75 @@
 package net.rubygrapefruit.ansi.html;
 
 import net.rubygrapefruit.ansi.Visitor;
-import net.rubygrapefruit.ansi.token.CarriageReturn;
-import net.rubygrapefruit.ansi.token.NewLine;
-import net.rubygrapefruit.ansi.token.Text;
-import net.rubygrapefruit.ansi.token.Token;
+import net.rubygrapefruit.ansi.token.*;
 
 /**
- * Formats a stream of {@link Token} instances into HTML. Does not handle control sequences. To do so, use an instance of this class as a parameter to {@link net.rubygrapefruit.ansi.console.AnsiConsole#contents(Visitor)}.
+ * Formats a stream of {@link Token} instances into HTML. Handles text attribute control sequences but does not handle cursor control sequences. To do so, use an instance of this class as a parameter to {@link net.rubygrapefruit.ansi.console.AnsiConsole#contents(Visitor)}.
  *
  * <p>Generates spans with the following classes:</p>
  * <ul>
+ *     <li>{@code ansi-bold}: bold text.</li>
+ *     <li>{@code ansi-sequence}: a sequence that is recognized but not interpreted.</li>
  *     <li>{@code ansi-unknown-sequence}: an unrecognized sequence.</li>
  * </ul>
  */
 public class HtmlFormatter implements Visitor {
     private final StringBuilder content = new StringBuilder();
+    private boolean bold;
+    private boolean spanHasContent;
 
     /**
      * Returns the current content as HTML.
      */
     public String toHtml() {
-        return "<!DOCTYPE html>\n<html>\n<head>\n<meta charset='UTF-8'>\n<style>\npre { font-family: monospace; }\n.ansi-unknown-sequence { color: white; background: red; }</style>\n</head>\n<body>\n<pre>" + content + "</pre>\n</body>\n</html>";
+        endCurrentSpan();
+
+        return "<!DOCTYPE html>\n<html>\n<head>\n<meta charset='UTF-8'>\n<style>\npre { font-family: monospace; }\n.ansi-bold { font-weight: bold; }\n.ansi-unknown-sequence { color: white; background: red; }\n.ansi-sequence { color: black; background: #c0c0c0; }</style>\n</head>\n<body>\n<pre>" + content + "</pre>\n</body>\n</html>";
     }
 
     @Override
     public void visit(Token token) {
         if (token instanceof Text) {
             Text text = (Text) token;
-            content.append(text.getText());
+            appendText(text.getText());
         } else if (token instanceof NewLine) {
-            content.append("\n");
+            appendText("\n");
         } else if (token instanceof CarriageReturn) {
-            content.append("\r");
-        } else {
+            appendText("\r");
+        } else if (token instanceof BoldOn){
+            if (bold) {
+                return;
+            }
+            bold = true;
+            spanHasContent = false;
+        } else if (token instanceof BoldOff){
+            endCurrentSpan();
+            bold = false;
+        } else if (token instanceof UnrecognizedControlSequence){
+            endCurrentSpan();
             content.append("<span class='ansi-unknown-sequence'>");
             token.appendDiagnostic(content);
             content.append("</span>");
+        } else {
+            endCurrentSpan();
+            content.append("<span class='ansi-sequence'>");
+            token.appendDiagnostic(content);
+            content.append("</span>");
         }
+    }
+
+    private void endCurrentSpan() {
+        if (spanHasContent && bold) {
+            content.append("</span>");
+            spanHasContent = false;
+        }
+    }
+
+    private void appendText(String chars) {
+        if (!spanHasContent && bold) {
+            content.append("<span class='ansi-bold'>");
+        }
+        content.append(chars);
+        spanHasContent = true;
     }
 }
--- src/test/groovy/net/rubygrapefruit/ansi/html/HtmlFormatterTest.groovy
+++ src/test/groovy/net/rubygrapefruit/ansi/html/HtmlFormatterTest.groovy
@@ -1,13 +1,21 @@
 package net.rubygrapefruit.ansi.html
 
+import net.rubygrapefruit.ansi.token.BoldOff
+import net.rubygrapefruit.ansi.token.BoldOn
 import net.rubygrapefruit.ansi.token.CursorBackward
 import net.rubygrapefruit.ansi.token.EraseInLine
 import net.rubygrapefruit.ansi.token.NewLine
 import net.rubygrapefruit.ansi.token.Text
+import net.rubygrapefruit.ansi.token.UnrecognizedControlSequence
 import spock.lang.Specification
 
 class HtmlFormatterTest extends Specification {
     def formatter = new HtmlFormatter()
+
+    def "formats empty content"() {
+        expect:
+        formatter.toHtml().contains("<pre></pre>")
+    }
 
     def "formats text"() {
         expect:
@@ -17,12 +25,57 @@ class HtmlFormatterTest extends Specification {
         formatter.toHtml().contains("<pre>123\n456</pre>")
     }
 
+    def "formats bold text"() {
+        expect:
+        formatter.visit(new Text("123"))
+        formatter.visit(BoldOn.INSTANCE)
+        formatter.visit(new Text("456"))
+        formatter.visit(BoldOff.INSTANCE)
+        formatter.visit(new Text("789"))
+        formatter.toHtml().contains("<pre>123<span class='ansi-bold'>456</span>789</pre>")
+    }
+
+    def "ignores bold control sequence that has no effect"() {
+        expect:
+        formatter.visit(BoldOff.INSTANCE)
+        formatter.visit(BoldOff.INSTANCE)
+        formatter.visit(new Text("123"))
+        formatter.visit(BoldOn.INSTANCE)
+        formatter.visit(BoldOff.INSTANCE)
+        formatter.visit(new Text("456"))
+        formatter.visit(BoldOn.INSTANCE)
+        formatter.visit(BoldOn.INSTANCE)
+        formatter.visit(BoldOff.INSTANCE)
+        formatter.visit(BoldOn.INSTANCE)
+        formatter.visit(new Text("789"))
+        formatter.toHtml().contains("<pre>123456<span class='ansi-bold'>789</span></pre>")
+    }
+
     def "formats control sequences"() {
         expect:
         formatter.visit(new CursorBackward(2))
         formatter.visit(new Text("456"))
         formatter.visit(NewLine.INSTANCE)
         formatter.visit(EraseInLine.INSTANCE)
-        formatter.toHtml().contains("<pre><span class='ansi-unknown-sequence'>{cursor-backward 2}</span>456\n<span class='ansi-unknown-sequence'>{erase-in-line}</span></pre>")
+        formatter.toHtml().contains("<pre><span class='ansi-sequence'>{cursor-backward 2}</span>456\n<span class='ansi-sequence'>{erase-in-line}</span></pre>")
+    }
+
+    def "formats unrecognized control sequences"() {
+        expect:
+        formatter.visit(new UnrecognizedControlSequence("[123m"))
+        formatter.visit(new Text("456"))
+        formatter.visit(NewLine.INSTANCE)
+        formatter.visit(new UnrecognizedControlSequence("[5m"))
+        formatter.toHtml().contains("<pre><span class='ansi-unknown-sequence'>{escape [123m}</span>456\n<span class='ansi-unknown-sequence'>{escape [5m}</span></pre>")
+    }
+
+    def "formats interleaved control sequences and text attributes"() {
+        expect:
+        formatter.visit(BoldOn.INSTANCE)
+        formatter.visit(new Text("123"))
+        formatter.visit(new CursorBackward(2))
+        formatter.visit(new Text("456"))
+        formatter.visit(new UnrecognizedControlSequence("[5m"))
+        formatter.toHtml().contains("<pre><span class='ansi-bold'>123</span><span class='ansi-sequence'>{cursor-backward 2}</span><span class='ansi-bold'>456</span><span class='ansi-unknown-sequence'>{escape [5m}</span></pre>")
     }
 }
