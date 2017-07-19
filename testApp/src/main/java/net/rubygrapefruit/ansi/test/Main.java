475ceef5eef60f0106e7a891d52534af1349c1e8
Adam Murdoch <adam@gradle.com>
2017-07-19 18:28:49 +1200

    Tweaks to test app output.

--- testApp/src/main/java/net/rubygrapefruit/ansi/test/Main.java
+++ testApp/src/main/java/net/rubygrapefruit/ansi/test/Main.java
@@ -63,18 +63,42 @@ public class Main {
         String cursorUp2 = "\u001B[2A";
         String cursorBack20 = "\u001B[20D";
         String cursorBack4 = "\u001B[4D";
+        String cursorBack8 = "\u001B[8D";
+        String cursorBack7 = "\u001B[7D";
         String eraseToEndLine = "\u001B[K";
+        String eraseToStartLine = "\u001B[1K";
         String red = "\u001B[0;31m";
-        String boldRed = "\u001B[31;1m";
         String reset = "\u001B[0m";
+        String underline = "\u001B[4m";
         String defaultColors = "\u001B[39;49m";
         String cursorUp1 = "\u001B[1A";
         String highBlack = "\u001B[0;90m";
         String white = "\u001B[0;37m";
+        String redBackground = "\u001B[41m";
         String blueBackground = "\u001B[44m";
         String brightBlueBackground = "\u001B[104m";
+
+        System.out.write((reset + "\n" + underline + boldOn + "CURSOR" + reset + "\n").getBytes(utf8));
         System.out.write(("\n\n" + cursorUp2 + "normal     " + boldOn + "bold" + boldOff + cursorDown2 + "\r" + red + "red        " + boldOn + "bold red" + cursorBack20 + cursorUp1 + highBlack + "high black " + boldOn + "bold high black\n\n").getBytes(utf8));
-        System.out.write((white + blueBackground + "white on blue    " + cursorBack4 + eraseToEndLine + "\n" + brightBlueBackground + "white on bright blue    " + defaultColors + eraseToEndLine + "\n\n").getBytes(utf8));
+
+        System.out.write((reset + "\n" + underline + boldOn + "BACKGROUND" + reset + "\n").getBytes(utf8));
+        System.out.write((white + blueBackground + "white on blue        " + defaultColors + "\n" + white + brightBlueBackground + "white on bright blue " + defaultColors + "\n").getBytes(utf8));
+
+        System.out.write((reset + "\n" + underline + boldOn + "ERASE" + reset + "\n").getBytes(utf8));
+        // Erase from middle of line to start
+        System.out.write(("12345<empty>" + redBackground + cursorBack8 + eraseToStartLine + defaultColors + "\n").getBytes(utf8));
+        // Erase from end of line to start
+        System.out.write(("1234" + redBackground + eraseToStartLine + defaultColors + "\n").getBytes(utf8));
+        // Erase from start of line to start
+        System.out.write(("12345\r" + redBackground + eraseToStartLine + defaultColors + "\n").getBytes(utf8));
+        // Erase from middle of line to end
+        System.out.write(("12345<empty>" + redBackground + cursorBack7 + eraseToEndLine + defaultColors + "\n").getBytes(utf8));
+        // Erase from start of line to end
+        System.out.write(("12345\r" + redBackground + eraseToEndLine + defaultColors + "\n").getBytes(utf8));
+        // Erase from end of line to end
+        System.out.write(("12345" + redBackground + eraseToEndLine + defaultColors + "\n").getBytes(utf8));
+
+        System.out.write((reset + "\n" + underline + boldOn + "COLORS" + reset + "\n").getBytes(utf8));
         System.out.write("\u001B[30mblack    \u001B[1mbold    \u001B[22;90mbright    \u001B[1mbold    \u001B[0;37;40mbackground    \u001B[100mbright    \u001B[0m\n".getBytes(utf8));
         System.out.write("\u001B[31mred      \u001B[1mbold    \u001B[22;91mbright    \u001B[1mbold    \u001B[0;37;41mbackground    \u001B[101mbright    \u001B[0m\n".getBytes(utf8));
         System.out.write("\u001B[32mgreen    \u001B[1mbold    \u001B[22;92mbright    \u001B[1mbold    \u001B[0;37;42mbackground    \u001B[102mbright    \u001B[0m\n".getBytes(utf8));
