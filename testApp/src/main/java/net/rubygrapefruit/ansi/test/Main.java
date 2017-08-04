404128ee8cd8b1647a5311f3fe18a3f42a158be0
Adam Murdoch <adam@gradle.com>
2017-08-04 12:05:19 +1000

    Javadocs.

--- testApp/src/main/java/net/rubygrapefruit/ansi/test/Main.java
+++ testApp/src/main/java/net/rubygrapefruit/ansi/test/Main.java
@@ -13,7 +13,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
- * A test app that can either generate some ANSI test output to stdout, or convert stdin to HTML.
+ * A test app that can either generate some ANSI test output to System.out, or convert System.in to HTML.
  */
 public class Main {
     public static void main(String[] args) throws IOException {
